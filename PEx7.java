f178d2f3cb306189058c10fe19a25555afa23eaf
pavani <vankayalapatipavani2003@gmail.com>
2024-08-09 23:54:51 +0530

    Different Types of Patterns in java

---
+++ PEx7.java
@@ -0,0 +1,16 @@
+class PEx7 
+{
+	public static void main(String[] args) 
+	{
+		int num = 1;
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j = 1; j<=5; j++)
+			{
+				System.out.print(num+"\t");
+				num++;
+			}
+			System.out.println();
+		}
+	}
+}
