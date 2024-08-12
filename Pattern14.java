28d4e90c261f645bc671e42541ebc27737c56308
pavani vankayalapati <pavani.vankayalapati16@gmail.com>
2024-08-12 20:20:38 +0530

    Different Types of Patterns in java

---
+++ Pattern14.java
@@ -0,0 +1,26 @@
+class Pattern14 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=5; i++)
+		{
+			for(int j = 1; j<=5; j++)
+			{
+				if(j<=i) // or (j<=i)
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
